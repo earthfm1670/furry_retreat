a9d375c3b8da52ef2d9960f4ef4e50754f680b11
Auttasit <auttasit@unixdev.co.th>
2025-01-28 19:07:36 +0700

    feat: add routing constants and create placeholder pages for authentication, booking, cart, payment, and user dashboard

---
+++ src/constants/index.ts
@@ -0,0 +1,11 @@
+export const ROUTES = {
+    LOGIN: "/auth/login",
+    REGISTER: "/auth/register",
+    BOOKING: "/booking",
+    CART: "/cart",
+    PAYMENT: "/payment",
+    PAYMENT_SUCCESS: "/paymentsuccess",
+    PRODUCTS: "/products",
+    SERVICES: "/services",
+    USER_DASHBOARD: "/userdashboard",
+}
